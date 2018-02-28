4133c4898028656d80c1d0bb001470793d5c4944
mamiaomiao <https://github.com/X970805/SocialApp.git>
2018-02-28 17:04:31 +0800

    图二相关功能，下拉刷新xrecyclerview的使用

--- monthpractice1/src/main/java/com/example/monthpractice1/view/SecondListActivity.java
+++ monthpractice1/src/main/java/com/example/monthpractice1/view/SecondListActivity.java
@@ -5,14 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import com.example.monthpractice1.R;
 import com.example.monthpractice1.adapter.GoodsAdapter;
 import com.example.monthpractice1.bean.GoodsBean;
 import com.example.monthpractice1.contract.IGoodsListView;
 import com.example.monthpractice1.presenter.GoodsListPresenter;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 import java.util.List;
 
@@ -21,23 +20,24 @@ import java.util.List;
  */
 
 public class SecondListActivity extends Activity implements IGoodsListView {
-    private RecyclerView recyclerView;
+    private XRecyclerView recyclerView;
     private GoodsAdapter adapter;
     private GoodsListPresenter presenter;
     private int pscid;
+    private int page = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         //从上一个列表页（图一）获取pscid
         Intent intent = getIntent();
         if (intent != null) {
             pscid = intent.getIntExtra("pscid", 0);
         }
         //隐藏左侧列表，只显示右侧列表
-        findViewById(R.id.left_list).setVisibility(View.GONE);
-        recyclerView = (RecyclerView) findViewById(R.id.right_list);
+        //findViewById(R.id.left_list).setVisibility(View.GONE);
+        recyclerView = (XRecyclerView) findViewById(R.id.xlist);
         adapter = new GoodsAdapter(SecondListActivity.this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(SecondListActivity.this));
@@ -46,13 +46,34 @@ public class SecondListActivity extends Activity implements IGoodsListView {
         presenter.attachView(this);
         //通过presenter请求数据并显示；
         if (pscid > 0) {
-            presenter.getData(pscid, 1);
+            presenter.getData(pscid, page);
         }
+
+        recyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                page = 1;
+                presenter.getData(pscid, page);
+            }
+
+            @Override
+            public void onLoadMore() {
+                page++;
+                presenter.getData(pscid, page);
+            }
+        });
     }
 
     @Override
     public void showGoodsList(List<GoodsBean.DataBean> data) {
-        //显示数据
-        adapter.addList(data);
+        recyclerView.refreshComplete();
+        recyclerView.loadMoreComplete();
+        if (page > 1) {
+            //显示添加数据
+            adapter.addList(data);
+        } else {
+            adapter.updateList(data);
+        }
+
     }
 }
